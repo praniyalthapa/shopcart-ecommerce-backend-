02d207c4bbb8824c194d6e154d051f5bfe606a7f
Praniyal Thapa <praniyalthapa100@gmail.com>
2024-07-22 21:15:10 +0545

    added some basic validator middlewares

--- src/middlewares/product_middleware.js
+++ src/middlewares/product_middleware.js
@@ -1,13 +1,21 @@
-// const errorResponse = require("../utils/error_response");
 
-// function createProductValidator(req, res, next) {
-//     console.log("Request Body in Middleware:", req.body); // Log the request body in middleware
 
-//     if (!req.body.title) {
-//         return res.json(errorResponse('discription is not present'),{message:'description is not present'});
-            
-//     }
-//     next();
-// }
+function createProductValidator(req, res, next) {
+   // console.log("Request Body in Middleware:", req.body); // Log the request body in middleware
 
-// module.exports = createProductValidator;
+    if (!req.body.title) {
+       // return res.json(errorResponse('discription is not present'),{message:'description is not present'});
+          return res.json({
+            success:false,
+            data:{},
+            message:'You have missed to input title parameter',
+            error:{
+                message:'Request body is missing so we gave you error!!Check it once!!Thank you!!'
+            }
+          })  
+    }
+    //if everthing looks good
+    next();
+}
+
+module.exports = {createProductValidator};
--- src/routes/v1/product_routes.js
+++ src/routes/v1/product_routes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const {createProduct}=require('../../controllers/product_controller');
+const {createProductValidator}=require('../../middlewares/product_middleware');
 const productRouter = express.Router();
 
-productRouter.post('/',createProduct); //this means first it validates from middleware and its next function will be controller.It maps routes to a controller
+productRouter.post('/',createProductValidator,createProduct); //this means first it validates from middleware and its next function will be controller.It maps routes to a controller
 
 
 
